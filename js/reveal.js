a308596130494b075efb4f69458b4a6345631220
fescii <isfescii@gmail.com>
2022-08-12 15:09:10 +0300

    js

---
+++ js/reveal.js
@@ -0,0 +1,19 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.welcome', { delay: 100 });
+ScrollReveal().reveal('.greeting', { delay: 120 });
+ScrollReveal().reveal('.section-title', { delay: 150 });
+ScrollReveal().reveal('.under-title', { delay: 180 });
+ScrollReveal().reveal('.welcome-article', { delay: 200 });
+
+ScrollReveal().reveal('.best', { delay: 100 });
+ScrollReveal().reveal('.best-article', { delay: 150 });
+
+ScrollReveal().reveal('.editors-pick', { delay: 100 });
+ScrollReveal().reveal('.editors-pick-article', { delay: 200 });
+
+ScrollReveal().reveal('.most-read', { delay: 100 });
+ScrollReveal().reveal('.most-read-article', { delay: 200 });
+
+
+ScrollReveal().reveal('.topics', { delay: 50 });
+
