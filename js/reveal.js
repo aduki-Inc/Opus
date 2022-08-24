02c1bad92bc50625a06e998842ac47356863e04e
fescii <isfescii@gmail.com>
2022-08-24 17:42:48 +0300

    Display: Entry A63

--- js/reveal.js
+++ js/reveal.js
@@ -1,14 +1,14 @@
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.welcome', { delay: 100 });
-ScrollReveal().reveal('.greeting', { delay: 150 });
+/*ScrollReveal().reveal('.greeting', { delay: 150 });
 ScrollReveal().reveal('.section-title', { delay: 200 });
 ScrollReveal().reveal('.under-title', { delay: 220 });
 ScrollReveal().reveal('.welcome-article', { delay: 250 });
 ScrollReveal().reveal('.article-title', { delay: 300 });
-ScrollReveal().reveal('.article-info', { delay: 350 });
+ScrollReveal().reveal('.article-info', { delay: 350 });*/
 
 ScrollReveal().reveal('.best', { delay: 100 });
-ScrollReveal().reveal('.best-article', { delay: 150 });
+ScrollReveal().reveal('.best-article', { delay: 200 });
 
 ScrollReveal().reveal('.editors-pick', { delay: 100 });
 ScrollReveal().reveal('.editors-pick-article', { delay: 200 });
@@ -17,5 +17,5 @@ ScrollReveal().reveal('.most-read', { delay: 100 });
 ScrollReveal().reveal('.most-read-article', { delay: 200 });
 
 
-ScrollReveal().reveal('.topics', { delay: 150 });
+ScrollReveal().reveal('.topics', { delay: 200 });
 
