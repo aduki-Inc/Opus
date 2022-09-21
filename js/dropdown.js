618c7c09c3c4979d4c8064733cd3ca6870effd7b
fescii <isfescii@gmail.com>
2022-09-21 21:46:26 +0300

    Feeds: Entry A39

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,14 +1,23 @@
 /* Humbugger Menu */
 const hamburgerMenu = document.querySelector('.hamburger'); //Selects the element with the class name of hamburger.
-    if(hamburgerMenu != null){
+const menu = document.querySelector('.dashboard-sidebar');
+if(hamburgerMenu != null){
         //Creating an arrow function for changing the class to active.
         const menuIsActive = () => {
         //Selecting the hamburger menu and adding and removing the class of active on click.
         hamburgerMenu.classList.toggle('active');
+        if(menu.style.display == 'block'){
+            menu.style.setProperty('display', 'none');
+        }
+        else{
+            menu.style.setProperty('display', 'block');
+        }
+
         };
         //Adds an event listener of 'click' to the hamburger menu. Then we pass in the function we created where the class is toggled on and off.
         hamburgerMenu.addEventListener('click', menuIsActive);
-    }
+
+}
 
 
 // Dropdown
